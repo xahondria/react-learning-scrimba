2fb506c57d2df97224eef39f985a240b7e1086b6
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2019-02-13 13:47:49 +0300

    completed exercises up to 33

--- src/33 Todo App - Phase 6/index.js
+++ src/33 Todo App - Phase 6/index.js
@@ -26,12 +26,22 @@ class App extends React.Component{
   }
 
   handleChange(id) {
-    // Update state so that the item with the given id flips `completed` from false to true (or vise versa)
-    // Remember not to modify prevState directly, but instead to return a new version of state with the change you want included in that update. (Think how you might use the `.map` method to do this)
+    this.setState((prevState) => {
+      const updaterTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+
+      return {
+        todos: updaterTodos
+      };
+    })
   }
 
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item}/>);
+    const todoItems = this.state.todos.map(item => <TodoItem key = {item.id} item = {item} handleChange = {this.handleChange}/>);
 
     return (
       <div className="todo-list">
--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Index22 from './22 Mapping Components Practice';
-import Index23 from './23 Todo App - Phase 3';
-import Index29 from './29 Todo App - Phase 4';
 import Index33 from './33 Todo App - Phase 6';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<Index22 />*/}
-        {/*<Index23/>*/}
-        {/*<Index29/>*/}
         <Index33/>
       </div>
     );
