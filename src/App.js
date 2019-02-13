962e818a4555a1e46a2ce876bbc3a6436e88e09e
Vladimir Kolomoyets <kolomoyetsva@gmail.com>
2019-02-13 12:00:42 +0300

    added exercise 33

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Index22 from './22 Mapping Components Practice/index22';
-import Index23 from './23 Todo App - Phase 3/index23';
-import Index29 from './29 Todo App - Phase 4/index29'
+import Index22 from './22 Mapping Components Practice';
+import Index23 from './23 Todo App - Phase 3';
+import Index29 from './29 Todo App - Phase 4';
+import Index33 from './33 Todo App - Phase 6';
 
 class App extends Component {
   render() {
@@ -11,7 +12,8 @@ class App extends Component {
       <div className="App">
         {/*<Index22 />*/}
         {/*<Index23/>*/}
-        <Index29/>
+        {/*<Index29/>*/}
+        <Index33/>
       </div>
     );
   }
